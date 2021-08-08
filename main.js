5356f8016212c0c3d9f5393a7bb80c3629e31a71
ember-code <knandor407@gmail.com>
2021-08-08 15:17:41 +0200

    Finished can't wait to learn about databases

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
 let myLibrary = [];
-//let numberOfBook=-1;
+let idpass=0;
 const titleTextElement=document.querySelector("#title");
 const authorTextElement=document.querySelector("#author");
 const pagesTextElement=document.querySelector("#pages");
 const addButtonTextElement=document.querySelector("#addButton");
 const readTextElement=document.querySelector("#read");
 const tbody=document.querySelector("tbody");
-
-clear();
 function clear(){
     titleTextElement.value="";
     authorTextElement.value="";
     pagesTextElement.value="";
+    readTextElement.checked=false;
+    readTextElement.value=false;
 }
-function Book(title, author, pages, read) {
+clear();
+
+function Book(title, author, pages, read,id) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id=id;
 }
 
-function addBookTolibrary(title, author, pages, read) {
-    let book=new Book(title, author, pages, read);
+function addBookTolibrary(title, author, pages, read,id) {
+    
+    let book=new Book(title, author, pages, read,id);
     myLibrary.push(book);
     console.table(myLibrary);
-    //numberOfBook++;
+    idpass++;
     addBookToHtml(title, author, pages, read);
 }
 function addBookToHtml(title, author, pages, read){
@@ -45,18 +49,35 @@ function addBookToHtml(title, author, pages, read){
     tr.appendChild(tdPages);
 
     let tdRead=document.createElement('td');
+    
     if(read=="true"){
         tdRead.textContent="Have read";
     }else{
         tdRead.textContent="Haven't read";
     }
-    
     tr.appendChild(tdRead);
+    tdRead.addEventListener("click",go);
+    function go(){
+        console.log("idpass" +idpass);
+        console.table("ezt "+myLibrary[idpass-1])
+        console.log("igaz*e "+myLibrary[idpass-1].read)
+        if(this.textContent=="Have read"){
+            this.textContent="Haven't read";
+            myLibrary[idpass-1].read=false;
+        }else{
+            this.textContent="Have read";
+            myLibrary[idpass-1].read=true;
+        }
+    }
+
+
+
 
     let buttonContainerTd=document.createElement('td');
     let deleteButton=document.createElement('button');
     let img=document.createElement('img');
     deleteButton.classList.add("deleteBtn");
+    deleteButton.setAttribute("data-id",idpass);
     deleteButton.appendChild(img);
     buttonContainerTd.appendChild(deleteButton);
     deleteButton.addEventListener("click",deleteRow)
@@ -65,7 +86,24 @@ function addBookToHtml(title, author, pages, read){
     clear();
 }
 function deleteRow(e){
+    e=this;
+    let jo="";
+    //console.log("ez "+e);
     this.parentNode.parentNode.remove();
+    const toRemove=myLibrary.filter((book)=>{
+        console.log("book id "+book.id);
+        let ennekid=e.dataset.id;
+        console.log("ennek id "+e.dataset.id)
+        if(book.id==ennekid-1){
+            console.log("itt az id  ")
+            jo=book.id;
+            return jo;
+        }
+
+    })
+    console.table("remove "+jo);
+    
+    myLibrary[jo]="";
     clear();
 }
 function validateForm(){
@@ -90,7 +128,7 @@ function validateForm(){
         pagesTextElement.placeholder="Please write a positive integer";
         return;
     }
-    addBookTolibrary(titleTextElement.value,authorTextElement.value,pagesTextElement.value,readTextElement.value)
+    addBookTolibrary(titleTextElement.value,authorTextElement.value,pagesTextElement.value,readTextElement.value,idpass)
 
 }
 /*addBookTolibrary("kl","béla","256",true);
@@ -100,6 +138,7 @@ console.log(myLibrary);*/
 
 addButtonTextElement.addEventListener("click",validateForm);
 readTextElement.addEventListener("change",function(){
+    console.log(this.checked);
     if(this.checked){
         readTextElement.value=true;
     }
